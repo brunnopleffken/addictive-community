fd330bb2e06a3aa466af7eaeac0a6ea17ea3ebfa
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-01-09 01:35:13 -0200

    Index now allows custom templates

--- index.php
+++ index.php
@@ -39,21 +39,34 @@
 
 		public function __construct()
 		{
+			// Intial configuration
+
 			include("config.php");
 
 			$init = new Init();
 			$init->Load();
 
+			// Define classes
+
 			$this->Db = new Database($config);
 			$this->Core = new Core($this->Db);
+
+			// Load views and controllers
 
 			ob_start();
 			require_once("controllers/" . $this->info['module'] . ".php");
 			require_once("templates/" . $this->info['template'] . "/" . $this->info['module'] . ".php");
 			$this->content = ob_get_clean();
 
-			require_once("templates/" . $this->info['template'] . "/default.php");
-			require_once("controllers/default.php");
+			if(isset($define['layout'])) {
+				$layout = $define['layout'];
+			}
+			else {
+				$layout = "default";
+			}
+
+			require_once("templates/" . $this->info['template'] . "/" . $layout . ".php");
+			require_once("controllers/" . $layout . ".php");
 		}
 	}
 
