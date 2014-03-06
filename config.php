6146c2cebd9e8283f039e70c5fe3109d7bf58914
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-03-06 14:49:49 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$config['db_server'] = "localhost";
-	$config['db_username'] = "root";
-	$config['db_password'] = "loremipsum";
-	$config['db_database'] = "community";
-	$config['db_prefix'] = "c_";
-
-?>
