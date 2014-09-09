857ed619d25edea12a42324d825e1ca3e907b882
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-09-09 18:32:40 -0300

    AC Installer Ajax is ready... TO DO: PHP/SQL

--- install/execute.php
+++ install/execute.php
@@ -1,12 +1,85 @@
 <?php
 
-	## ---------------------------------------------------
-	#  HOMEFRONT ONLINE
-	## ---------------------------------------------------
-	#  Developed by Brunno Pleffken Hosti
-	#  File: execute.php
-	#  Release: v1.0.0
-	#  Copyright: (c) 2014 - Homefront Interactive
-	## ---------------------------------------------------
+	// ---------------------------------------------------
+	//  ADDICTIVE COMMUNITY
+	// ---------------------------------------------------
+	// Created by Brunno Pleffken Hosti
+	//
+	// Website: www.addictive.com.br
+	// E-mail: brunno.pleffken@addictive.com.br
+	// Release: December/2012
+	// ---------------------------------------------------
+
+	$step = $_REQUEST['step'];
+	$data = $_POST;
+
+	switch($step) {
+		// --------------------------------------------
+		// Save configuration file
+		// --------------------------------------------
+		case 1:
+			if(is_writable("../config.php")) {
+				// ...
+				$status = 1;
+			}
+			else {
+				$status = 0; // Error
+			}
+
+			$description = "Save configuration file " . $_POST['community_name'];
+			break;
+
+		// --------------------------------------------
+		// Check information and connect to database
+		// --------------------------------------------
+		case 2:
+			$status      = 1;
+			$description = "Check information and connect to database";
+			break;
+
+		// --------------------------------------------
+		// Extract table structure
+		// --------------------------------------------
+		case 3:
+			$status      = 1;
+			$description = "Extract table structure";
+			break;
+
+		// --------------------------------------------
+		// Insert initial data
+		// --------------------------------------------
+		case 4:
+			$status      = 1;
+			$description = "Insert initial data";
+			break;
+
+		// --------------------------------------------
+		// Save user information
+		// --------------------------------------------
+		case 5:
+			$status      = 1;
+			$description = "Save user information";
+			break;
+
+		// --------------------------------------------
+		// Lock installer
+		// --------------------------------------------
+		case 6:
+			$status      = 1;
+			$description = "Lock installer";
+			break;
+
+		default:
+			$status = 0;
+	}
+
+	$data = array(
+		'status' => $status,
+		'step'   => $step,
+		'time'   => microtime(),
+		'desc'   => $description
+	);
+
+	echo json_encode($data);
 
 ?>
--- install/index.php
+++ install/index.php
@@ -4,16 +4,16 @@
 	//  ADDICTIVE COMMUNITY
 	// ---------------------------------------------------
 	// Created by Brunno Pleffken Hosti
-	// 
+	//
 	// Website: www.addictive.com.br
 	// E-mail: brunno.pleffken@addictive.com.br
 	// Release: December/2012
 	// ---------------------------------------------------
-	
+
 	// --------------------------------------------
 	// Initialization information
 	// --------------------------------------------
-	
+
 	require_once("../init.php");
 	require_once("../kernel/class.html.php");
 	require_once("../kernel/class.database.php");
@@ -26,34 +26,34 @@
 	// --------------------------------------------
 	// Installer class inherits main Database class
 	// --------------------------------------------
-	
+
 	class Installer extends Database
 	{
 		public $input = array();
-		
+
 		public function InstallerDB()
 		{
-			$this->mysql['host']	 = $this->input['db_server'];
-			$this->mysql['user']	 = $this->input['db_username'];
+			$this->mysql['host']     = $this->input['db_server'];
+			$this->mysql['user']     = $this->input['db_username'];
 			$this->mysql['password'] = $this->input['db_password'];
-			$this->mysql['dbase']	 = $this->input['db_database'];
-			
+			$this->mysql['dbase']    = $this->input['db_database'];
+
 			$config = $this->input;
 			$this->_Connect($config);
 		}
 	}
-	
+
 	// --------------------------------------------
 	// Which step is the user in
 	// --------------------------------------------
-	
+
 	if(isset($_REQUEST['step'])) {
 		$step = $_REQUEST['step'];
 	}
 	else {
 		$step = 1;
 	}
-	
+
 	if(file_exists("eula.txt") && $step == 1) {
 		$eula = file_get_contents("eula.txt");
 	}
@@ -65,13 +65,13 @@
 
 switch($step)
 {
-	
+
 	// --------------------------------------------
 	// Step 1
 	// --------------------------------------------
-	
+
 	case 1:
-		
+
 		$tpl = <<<HTML
 		<div class="step-box">
 			<div class="step-box-current"><h3>Step 1</h3><span class="tiny">EULA</span></div>
@@ -80,7 +80,7 @@ switch($step)
 			<div class="step-box-next"><h3>Step 4</h3><span class="tiny">Community Settings</span></div>
 			<div class="step-box-next"><h3>Step 5</h3><span class="tiny">Install</span></div>
 		</div>
-		
+
 		<form method="post" name="install">
 			<div style="text-align: center">
 				<textarea style="width: 550px; height: 300px; margin-bottom: 20px" readonly>{$eula}</textarea>
@@ -93,15 +93,15 @@ switch($step)
 			</div>
 		</form>
 HTML;
-		
+
 		break;
-	
+
 	// --------------------------------------------
 	// Step 2
 	// --------------------------------------------
-	
+
 	case 2:
-		
+
 		$tpl = <<<HTML
 		<div class="step-box">
 			<div class="step-box-previous"><h3>Step 1</h3><span class="tiny">EULA</span></div>
@@ -110,9 +110,9 @@ HTML;
 			<div class="step-box-next"><h3>Step 4</h3><span class="tiny">Community Settings</span></div>
 			<div class="step-box-next"><h3>Step 5</h3><span class="tiny">Install</span></div>
 		</div>
-		
+
 		<form action="index.php?step=3" method="post" class="validate">
-		
+
 			<div class="input-box">
 				<div class="input-box-label">MySQL Host</div>
 				<div class="input-box-field"><input type="text" name="host" class="required medium"></div>
@@ -128,44 +128,44 @@ HTML;
 				<div class="input-box-field"><input type="password" name="password" class="required small"></div>
 			</div>
 			<div class="input-box" style="text-align: center"><input type="submit" value="Proceed"></div>
-		
+
 		</form>
 HTML;
-		
+
 		break;
-		
+
 	// --------------------------------------------
 	// Step 3
 	// --------------------------------------------
-	
+
 	case 3:
-		
+
 		session_start();
-		
+
 		// Connect to database and get information
 		$installer = new Installer;
-		
+
 		$_SESSION['db_server']   = $installer->input['db_server']   = $_REQUEST['host'];
 		$_SESSION['db_database'] = $installer->input['db_database'] = $_REQUEST['database'];
 		$_SESSION['db_username'] = $installer->input['db_username'] = $_REQUEST['username'];
 		$_SESSION['db_password'] = $installer->input['db_password'] = $_REQUEST['password'];
 
 		$installer->InstallerDB();
-		
+
 		$installer->Query("SELECT VERSION() AS mysql_version;");
 		$result = $installer->Fetch();
 
 		// Check system environment
-		
+
 		preg_match('#[0-9]+\.[0-9]+\.[0-9]+#', $result['mysql_version'], $mysql_version);
-		
+
 		$info['php-version'] = PHP_VERSION;
 		$info['mysql-version'] = $mysql_version[0];
 		$info['memory-limit'] = @ini_get("memory_limit");
-		
+
 		$php_v = version_compare($info['php-version'], MIN_PHP_VERSION);
 		$php_check = ($php_v >= 0) ? "<span style=\"color: #090\">Yes ({$info['php-version']})</span>" : "<span style=\"color: #900\">No ({$info['php-version']})</span>";
-		
+
 		$sql_v = version_compare($info['mysql-version'], MIN_SQL_VERSION);
 		$sql_check = ($sql_v >= 0) ? "<span style=\"color: #090\">Yes (v{$info['mysql-version']})</span>" : "<span style=\"color: #900\">No ({$info['mysql-version']})</span>";
 
@@ -174,12 +174,12 @@ HTML;
 		$environment .= "<tr><td>PHP 5.3+</td><td>{$php_check}</td></tr>";
 		$environment .= "<tr><td>MySQL 5.1+</td><td>{$sql_check}</td></tr>";
 		$environment .= "</table>";
-		
+
 		// Check extensions
-		
+
 		$extensions = get_loaded_extensions();
 		$required = array("gd", "libxml", "json");
-		
+
 		$ext_name = array(
 			"gd"		=> "GD Library",
 			"libxml"	=> "DOM XML Handling",
@@ -187,7 +187,7 @@ HTML;
 		);
 
 		$extensions_ok = "<table class=\"table\" style=\"width: 300px\">";
-		
+
 		foreach($required as $data) {
 			if(in_array($data, $extensions)) {
 				$status = "<span style=\"color: #090\">Yes</span>";
@@ -195,15 +195,15 @@ HTML;
 			else {
 				$status = "<span style=\"color: #C00\">No</span>";
 			}
-			
+
 			$extensions_ok .= "<tr><td>" . $ext_name[$data] . " ({$data})</td><td>{$status}</td></tr>";
 		}
-		
+
 		$extensions_ok .= "</table>";
-		
+
 		// Check folders
 		$disabled = "";
-	
+
 		// root/config.php
 		if(is_writable("../config.php")) {
 			$file_conf = "<span style=\"color: #090\">Writable</span>";
@@ -212,7 +212,7 @@ HTML;
 			$file_conf = "<span style=\"color: #C00\">Not writable</span>";
 			$disabled = "disabled='disabled'";
 		}
-	
+
 		// root/public/attachments
 		if(is_writable("../uploads/")) {
 			$dir_uploads = "<span style=\"color: #090\">Writable</span>";
@@ -221,7 +221,7 @@ HTML;
 			$dir_uploads = "<span style=\"color: #C00\">Not writable</span>";
 			$disabled = "disabled='disabled'";
 		}
-	
+
 		// root/public/attachments
 		if(is_writable("../public/attachments/")) {
 			$dir_attach = "<span style=\"color: #090\">Writable</span>";
@@ -230,7 +230,7 @@ HTML;
 			$dir_attach = "<span style=\"color: #C00\">Not writable</span>";
 			$disabled = "disabled='disabled'";
 		}
-	
+
 		// root/public/avatar
 		if(is_writable("../public/avatar/")) {
 			$dir_avatar = "<span style=\"color: #090\">Writable</span>";
@@ -239,17 +239,17 @@ HTML;
 			$dir_avatar = "<span style=\"color: #C00\">Not writable</span>";
 			$disabled = "disabled='disabled'";
 		}
-		
+
 		$folders = "<table class=\"table\" style=\"width: 300px\">";
 		$folders .= "<tr><td>/config.php</td><td>{$file_conf}</td></tr>";
 		$folders .= "<tr><td>/uploads</td><td>{$dir_uploads}</td></tr>";
 		$folders .= "<tr><td>/public/attachments</td><td>{$dir_attach}</td></tr>";
 		$folders .= "<tr><td>/public/avatar</td><td>{$dir_avatar}</td></tr>";
 		$folders .= "</table>";
-			
-		
+
+
 		// Do template!
-		
+
 		$tpl = <<<HTML
 		<div class="step-box">
 			<div class="step-box-previous"><h3>Step 1</h3><span class="tiny">EULA</span></div>
@@ -258,43 +258,43 @@ HTML;
 			<div class="step-box-next"><h3>Step 4</h3><span class="tiny">Community Settings</span></div>
 			<div class="step-box-next"><h3>Step 5</h3><span class="tiny">Install</span></div>
 		</div>
-		
+
 		<form action="index.php?step=3" method="post">
-			
+
 			<div class="input-box">
 				<h4>System Environment</h4>
 				{$environment}
 			</div>
-			
+
 			<div class="input-box">
 				<h4>Extensions</h4>
 				{$extensions_ok}
 			</div>
-			
+
 			<div class="input-box">
 				<h4>Files and Folders</h4>
 				{$folders}
 			</div>
-			
+
 			<div class="input-box" style="text-align: center"><input type="button" value="Proceed" onclick="javascript:window.location.replace('index.php?step=4')" {$disabled}></div>
 
 		</form>
 HTML;
-		
+
 		break;
-	
+
 	// --------------------------------------------
 	// Step 4
 	// --------------------------------------------
-	
+
 	case 4:
-		
+
 		session_start();
-		
+
 		$dir = str_replace("install", "", getcwd());
 		$url = str_replace("install/index.php", "", $_SERVER['HTTP_REFERER']);
 		$url = preg_replace("#\?(.+?)*#", "", $url);
-		
+
 		$tpl = <<<HTML
 		<div class="step-box">
 			<div class="step-box-previous"><h3>Step 1</h3><span class="tiny">EULA</span></div>
@@ -303,16 +303,16 @@ HTML;
 			<div class="step-box-current"><h3>Step 4</h3><span class="tiny">Community Settings</span></div>
 			<div class="step-box-next"><h3>Step 5</h3><span class="tiny">Install</span></div>
 		</div>
-		
+
 		<form action="index.php?step=5" method="post">
-		
+
 			<div class="input-box">
 				<div class="input-box-label">Community Name</div>
 				<div class="input-box-field"><input type="text" name="community" class="medium"></div>
 			</div>
-			
+
 			<h2>Paths and URLs</h2><br>
-			
+
 			<div class="input-box">
 				<div class="input-box-label">Installation Path</div>
 				<div class="input-box-field"><input type="text" name="adm_username" class="medium" value="{$dir}"></div>
@@ -321,9 +321,9 @@ HTML;
 				<div class="input-box-label">Installation URL</div>
 				<div class="input-box-field"><input type="text" name="adm_username" class="medium" value="{$url}"></div>
 			</div>
-			
+
 			<h2>Administrator Account</h2><br>
-			
+
 			<div class="input-box">
 				<div class="input-box-label">Username</div>
 				<div class="input-box-field"><input type="text" name="adm_username" class="small"></div>
@@ -338,7 +338,7 @@ HTML;
 				<div class="input-box-label">E-mail</div>
 				<div class="input-box-field"><input type="text" name="adm_email" class="medium"></div>
 			</div>
-			
+
 			<div class="input-box" style="text-align: center">
 				<input type="hidden" name="db_server" value="{$_SESSION['db_server']}">
 				<input type="hidden" name="db_database" value="{$_SESSION['db_database']}">
@@ -346,25 +346,31 @@ HTML;
 				<input type="hidden" name="db_password" value="{$_SESSION['db_password']}">
 				<input type="submit" value="Proceed">
 			</div>
-		
+
 		</form>
 HTML;
-		
+
 		session_destroy();
-		
+
 		break;
-	
+
 	// --------------------------------------------
 	// Step 5
 	// --------------------------------------------
-	
+
 	case 5:
-		
+
 		echo "<pre>";
 		print_r($_REQUEST);
 		echo "</pre>";
-		
+
 		$tpl = <<<HTML
+		<script type="text/javascript">
+			$(document).ready(function() {
+				installModule(1);
+			});
+		</script>
+
 		<div class="step-box">
 			<div class="step-box-previous"><h3>Step 1</h3><span class="tiny">EULA</span></div>
 			<div class="step-box-previous"><h3>Step 2</h3><span class="tiny">Database Settings</span></div>
@@ -372,9 +378,18 @@ HTML;
 			<div class="step-box-previous"><h3>Step 4</h3><span class="tiny">Community Settings</span></div>
 			<div class="step-box-current"><h3>Step 5</h3><span class="tiny">Install</span></div>
 		</div>
-		
+
+		<input type="hidden" id="db_server" value="{$_REQUEST['db_server']}">
+		<input type="hidden" id="db_database" value="{$_REQUEST['db_database']}">
+		<input type="hidden" id="db_username" value="{$_REQUEST['db_username']}">
+		<input type="hidden" id="db_password" value="{$_REQUEST['db_password']}">
+		<input type="hidden" id="community_name" value="{$_REQUEST['community']}">
+		<input type="hidden" id="admin_username" value="{$_REQUEST['adm_username']}">
+		<input type="hidden" id="admin_password" value="{$_REQUEST['adm_password']}">
+		<input type="hidden" id="admin_email" value="{$_REQUEST['adm_email']}">
+
 		<h4>Installation Progress</h4>
-		
+
 		<div id="log" style="line-height: 1.4em">
 			<div class="step1">Saving configuration file... <span class="ok">OK</span><span class="failed">FAILED</span></div>
 			<div class="step2">Checking information and connecting to database... <span class="ok">OK</span><span class="failed">FAILED</span></div>
@@ -382,13 +397,12 @@ HTML;
 			<div class="step4">Inserting initial data... <span class="ok">OK</span><span class="failed">FAILED</span></div>
 			<div class="step5">Saving user information... <span class="ok">OK</span><span class="failed">FAILED</span></div>
 			<div class="step6">Locking installer... <span class="ok">OK</span><span class="failed">FAILED</span></div>
-			<div class="step7">Locking installer... <span class="ok">OK</span><span class="failed">FAILED</span></div>
 			<input type="submit" value="Let's Go!" style="margin-top: 10px">
 		</div>
 HTML;
-		
+
 		break;
-		
+
 }
 
 // --------------------------------------------
@@ -411,7 +425,7 @@ HTML;
 	<script type="text/javascript">
 		// Addictive Community Installer - Javascripts
 		// Written by Brunno Pleffken
-		
+
 		function eula() {
 			checkbox = document.getElementById("agree");
 			if(checkbox.checked == false) {
@@ -422,11 +436,11 @@ HTML;
 				window.location.replace("index.php?step=2");
 			}
 		}
-		
+
 		function checkPasswordMatch() {
 			password = document.getElementById("adm_password");
 			confirm = document.getElementById("adm_password2");
-			
+
 			if(confirm.value != "") {
 				if(confirm.value != password.value) {
 					alert("Administrator passwords does not match!");
@@ -439,28 +453,52 @@ HTML;
 		}
 
 		// Run installer
-		
-		$(document).ready(function() {
-			function installModule(id) {
-				$.ajax({
-					url: 'execute.php?step=' + id,
-					type: 'get',
-					beforeSend: function(){
-						console.log("Initializing step " + id);
-					}
-				})
-				.done(function() {
-					console.log("Step " + id + ", success!");
-				})
-				.fail(function() {
-					console.log("Step " + id + ", error!");
-				})
-				.always(function() {
-					console.log("Step " + id + ", complete!");
-				});
-				
-			}
-		});
+
+		function installModule(id) {
+			var installData = {
+				db_server: $('#db_server').val(),
+				db_database: $('#db_database').val(),
+				db_username: $('#db_username').val(),
+				db_password: $('#db_password').val(),
+				community_name: $('#community_name').val(),
+				admin_username: $('#admin_username').val(),
+				admin_password: $('#admin_password').val(),
+				admin_email: $('#admin_email').val(),
+			};
+
+			$.ajax({
+				url: 'execute.php?step=' + id,
+				dataType: 'json',
+				type: 'post',
+				data: installData,
+				beforeSend: function() {
+					console.log("Initializing step " + id);
+					$('.step' + id).show();
+				}
+			})
+			.done(function(data) {
+				console.log(data);
+				console.log("Step " + id + ", success!");
+
+				if(data.status == 1) {
+					$('.step' + id + ' .ok').show();
+					id++;
+					installModule(id);
+				}
+				else {
+					$('.step' + id + ' .failed').show();
+				}
+			})
+			.fail(function() {
+				console.log("Step " + id + ", error!");
+				$('.step' + id + ' .failed').show();
+			})
+			.always(function() {
+				if(id == 7) {
+					$('#log input').fadeIn();
+				}
+			});
+		}
 	</script>
 	<style type="text/css">
 		#log > div { display: none; }
@@ -480,28 +518,28 @@ HTML;
 			<div class="fix"></div>
 		</div>
 	</div>
-	
+
 	<div class="wrapper">
-	
+
 		<div id="logo">
 			<div class="wrapper">
 				<img src="../admin/images/logo.png" class="logo-image">
 			</div>
 		</div>
-		
-		
+
+
 		<div id="content" style="width: 700px">
 			<?php echo $tpl; ?>
 		</div>
-		
+
 	</div>
-	
+
 	<div id="footer">
 		<div class="wrapper">
 			<span class="fright">Powered by Addictive Community &copy; <?php echo date("Y") ?> - All rights reserved.</span>
 		</div>
 	</div>
 
-	
+
 </body>
 </html>
