78a471ad3bc69de79c26768a1b86123e3c7c5700
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-09-18 09:38:29 -0300

    Remove config.php development values

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$config['db_server'] = "localhost";
-	$config['db_username'] = "root";
-	$config['db_password'] = "root";
-	$config['db_database'] = "community";
-	$config['db_prefix'] = "c_";
-
-?>
