0b818c3432dd4a4a9966b8865e6ad3aee95f2e17
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-11-18 23:27:41 -0200

    Validate E-mail settings in Email() constructor

--- kernel/class.email.php
+++ kernel/class.email.php
@@ -30,6 +30,18 @@
 
 			$this->Mail = new PHPMailer();
 
+			// Check if e-mail information is empty
+			
+			if($config['general_email_smtp'] == "" ||
+			   $config['general_email_authentication'] == "" ||
+			   $config['general_email_username'] == "" ||
+			   $config['general_email_password'] == ""
+			) {
+				Html::Error("<b>Mailer Error:</b> You're unable to send e-mails. Check your SMTP settings in "
+					. "<a href='admin/' target='_blank'>Administration Control Panel</a>."
+				);
+			}
+
 			// Set SMTP configuration
 			
 			$this->Mail->isSMTP();
