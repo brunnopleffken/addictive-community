5d6f8364d2b1a8a4fca588d15182b3c2e4acebbd
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-12-21 00:45:15 -0200

    Template 'room' rebuilt using i18n method

---
+++ languages/en_US/room.php
@@ -0,0 +1,14 @@
+<?php
+	$t["R_NEW_THREAD"] = "New Thread";
+	$t["R_ALL"] = "All";
+	$t["R_MY_THREADS"] = "My Threads";
+	$t["R_LAST_REPLY"] = "Last Reply";
+	$t["R_TOP_THREADS"] = "Top Threads";
+	$t["R_NO_REPLIES"] = "No Replies";
+	$t["R_ANSWERED"] = "Answered";
+	$t["R_STARTED_BY"] = "Started by %s";
+	$t["R_STARTED_ON"] = "On %s";
+	$t["R_VIEWS"] = "%d views";
+	$t["R_LAST_POST_BY"] = "Last post by %s";
+	$t["R_NO_THREADS"] = "There are no threads to be shown. What about starting a new one?";
+?>
--- templates/default/room.tpl.php
+++ templates/default/room.tpl.php
@@ -1,7 +1,7 @@
 <div class="roomTitleBar">
 	<div class="title fleft"><span><?php __($this->Core->config['general_communityname']) ?></span><?php __($roomInfo['name']) ?></div>
 	<?php if($this->IsMember()): ?>
-		<div class="buttons fright"><a href="index.php?module=newthread&amp;room=<?php __($roomId) ?>" class="defaultButton transition">New Thread</a></div>
+		<div class="buttons fright"><a href="?module=newthread&amp;room=<?php __($roomId) ?>" class="defaultButton transition"><?php __("R_NEW_THREAD") ?></a></div>
 	<?php endif; ?>
 </div>
 
@@ -12,14 +12,14 @@
 
 <div class="navigation">
 	<div class="navbar">
-		<a href="index.php?module=room&amp;id=<?php __($roomId) ?>" class="transition <?php __($menu[0]) ?>">All</a>
+		<a href="?module=room&amp;id=<?php __($roomId) ?>" class="transition <?php __($menu[0]) ?>"><?php __("R_ALL") ?></a>
 		<?php if($this->IsMember()) __($myThreadsMenu) ?>
 	</div>
 	<div class="subnav">
-		<a href="index.php?module=room&amp;id=<?php __($roomId) ?>" class="transition">Last Reply</a>
-		<a href="index.php?module=room&amp;id=<?php __($roomId) ?>&amp;act=topreplies" class="transition">Top Threads</a>
-		<a href="index.php?module=room&amp;id=<?php __($roomId) ?>&amp;act=noreplies" class="transition">No Replies</a>
-		<a href="index.php?module=room&amp;id=<?php __($roomId) ?>&amp;act=bestanswered" class="transition">Answered</a>
+		<a href="?module=room&amp;id=<?php __($roomId) ?>" class="transition"><?php __("R_LAST_REPLY") ?></a>
+		<a href="?module=room&amp;id=<?php __($roomId) ?>&amp;act=topreplies" class="transition"><?php __("R_TOP_THREADS") ?></a>
+		<a href="?module=room&amp;id=<?php __($roomId) ?>&amp;act=noreplies" class="transition"><?php __("R_NO_REPLIES") ?></a>
+		<a href="?module=room&amp;id=<?php __($roomId) ?>&amp;act=bestanswered" class="transition"><?php __("R_ANSWERED") ?></a>
 	</div>
 </div>
 
@@ -35,22 +35,22 @@
 			<?php __($_thread[$k]['author_avatar']) ?>
 		</td>
 		<td class="middle">
-			<a href="index.php?module=thread&amp;id=<?php __($_thread[$k]['t_id']) ?>" class="title"><?php __($_thread[$k]['title']) ?></a>
+			<a href="?module=thread&amp;id=<?php __($_thread[$k]['t_id']) ?>" class="title"><?php __($_thread[$k]['title']) ?></a>
 			<div class="desc"><?php __($_thread[$k]['description']) ?></div>
 			<div class="author">
 				<i class="fa fa-user"></i>
-				Started by <?php __($_thread[$k]['author_name']) ?> &nbsp;
+				<?php __("R_STARTED_BY", array($_thread[$k]['author_name'])) ?> &nbsp;
 				<i class="fa fa-clock-o"></i>
-				On <?php __($_thread[$k]['start_date']) ?>
+				<?php __("R_STARTED_ON", array($_thread[$k]['start_date'])) ?>
 			</div>
 		</td>
 		<td class="info"><i class="fa fa-comments"></i><br><?php __($_thread[$k]['replies']) ?></td>
 		<td class="stats">
 			<div class="label">
-				<i class="fa fa-fw fa-eye"></i><span class="value"><?php __($_thread[$k]['views']) ?> views</span>
+				<i class="fa fa-fw fa-eye"></i><span class="value"><?php __("R_VIEWS", array($_thread[$k]['views'])) ?></span>
 			</div>
 			<div class="label">
-				<i class="fa fa-fw fa-user"></i><span class="value"><a href="index.php?module=profile&amp;id=<?php __($_thread[$k]['lastpost_member_id']) ?>" title="Last post by <?php __($_thread[$k]['lastpost_name']) ?>"><?php __($_thread[$k]['lastpost_name']) ?></a></span>
+				<i class="fa fa-fw fa-user"></i><span class="value"><a href="?module=profile&amp;id=<?php __($_thread[$k]['lastpost_member_id']) ?>" title="<?php __("R_LAST_POST_BY", array($_thread[$k]['lastpost_name'])) ?>"><?php __($_thread[$k]['lastpost_name']) ?></a></span>
 			</div>
 			<div class="label">
 				<i class="fa fa-fw fa-clock-o"></i><span class="value"><?php __($_thread[$k]['lastpost_date']) ?></span>
@@ -66,7 +66,7 @@
 ?>
 
 <div class="threadItem">
-	<div class="center">There are no threads to be shown. What about starting a new one?</div>
+	<div class="center"><?php __("R_NO_THREADS") ?></div>
 </div>
 
 <?php
@@ -75,6 +75,6 @@
 
 <div class="roomTitleBar">
 	<?php if($this->IsMember()): ?>
-		<div class="buttons fright"><a href="index.php?module=newthread&amp;room=<?php __($roomId) ?>" class="defaultButton transition">New Thread</a></div>
+		<div class="buttons fright"><a href="?module=newthread&amp;room=<?php __($roomId) ?>" class="defaultButton transition"><?php __("R_NEW_THREAD") ?></a></div>
 	<?php endif; ?>
 </div>
