a7e2d75f376090d7203af64e4021a09bd7814c09
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-12-22 01:39:04 -0200

    Abuse report internationalization

---
+++ languages/en_US/report.php
@@ -0,0 +1,15 @@
+<?php
+	$t["R_TITLE"] = "Report Abuse";
+	$t["R_CLOSE"] = "Close";
+	$t["R_NOTICE"] = "Please, describe the abusive behavior exhibited.<br>All reports are kept strictly confidential.";
+	$t["R_REASON"] = "Reason";
+	$t["R_OPTION_CHOOSE"] = "Chose one...";
+	$t["R_OPTION_1"] = "Nudity or pornography";
+	$t["R_OPTION_2"] = "Impersonating me or someone I know";
+	$t["R_OPTION_3"] = "Racist or hate speech";
+	$t["R_OPTION_4"] = "Targets me or a friend";
+	$t["R_OPTION_5"] = "Direct call for violence";
+	$t["R_OPTION_6"] = "Excessive violent content";
+	$t["R_OPTION_7"] = "Spam";
+	$t["R_SEND"] = "Send";
+?>
--- templates/default/report.tpl.php
+++ templates/default/report.tpl.php
@@ -2,22 +2,36 @@
 	<table class="tableList noBorders" style="width:350px; margin:0">
 		<tr>
 			<th colspan="7">
-				<div class="fleft">Report Abuse</div>
-				<div class="fright"><a href="javascript:jQuery.fancybox.close();" class="smallButton grey white transition">Close</a></div>
+				<div class="fleft"><?php __("R_TITLE") ?></div>
+				<div class="fright"><a href="javascript:jQuery.fancybox.close();" class="smallButton grey white transition"><?php __("R_CLOSE") ?></a></div>
 			</th>
 		</tr>
 		<tr>
-			<td class="min">What's wrong with the selected post/thread (optional)?</td>
+			<td class="min"><?php __("R_NOTICE") ?></td>
 		</tr>
 		<tr>
-			<td><textarea name="description" class="large required" rows="6"></textarea></td>
+			<td><?php __("R_REASON") ?>:
+				<select name="" id="">
+					<option value="0" disabled="disabled"><?php __("R_OPTION_CHOOSE") ?></option>
+					<option value="1"><?php __("R_OPTION_1") ?></option>
+					<option value="2"><?php __("R_OPTION_2") ?></option>
+					<option value="3"><?php __("R_OPTION_3") ?></option>
+					<option value="4"><?php __("R_OPTION_4") ?></option>
+					<option value="5"><?php __("R_OPTION_5") ?></option>
+					<option value="6"><?php __("R_OPTION_6") ?></option>
+					<option value="7"><?php __("R_OPTION_7") ?></option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td><textarea name="description" class="large required" rows="6" style="resize:none"></textarea></td>
 		</tr>
 		<tr class="footer">
 			<td colspan="2" style="text-align:center; padding:10px">
 				<input type="hidden" name="m_id" value="<?php echo $this->Session->sInfo['member_id'] ?>">
 				<input type="hidden" name="t_id" value="<?php echo $threadId ?>">
 				<input type="hidden" name="p_id" value="<?php echo $postId ?>">
-				<input type="submit" value="Send">
+				<input type="submit" value="<?php __("R_SEND") ?>">
 			</td>
 		</tr>
 	</table>
