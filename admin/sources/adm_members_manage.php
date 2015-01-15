b962400cd32ef47ad6aeaf871b7a2dfb1293c17c
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-01-15 17:33:20 -0200

    Front-End adjustments on Manage Member settings

--- admin/sources/adm_members_manage.php
+++ admin/sources/adm_members_manage.php
@@ -8,69 +8,69 @@
 	#  Release: v1.0.0
 	#  Copyright: (c) 2014 - Addictive Software
 	## ---------------------------------------------------
-	
+
 	// Execute queries, if defined
-	
-	$execute = (Html::Request("execute")) ? Html::Request("execute") : false;
-	
-	if($execute) {
-		switch($execute) {
-			case "optimize":
-				
+
+	$do = (Html::Request("do")) ? Html::Request("do") : false;
+
+	if($do) {
+		switch($do) {
+			// Edit member info
+			case "edit":
+
+				break;
+
+			// Delete member
+			case "delete":
+
 				break;
 		}
 	}
-	
+
 	// Get member list
-	
-	$Db->Query("SELECT * FROM c_members
-		INNER JOIN c_usergroups ON (c_members.usergroup = c_usergroups.g_id);");
-	
+
+	$Db->Query("SELECT m.m_id, m.username, m.email, m.joined, m.posts, m.usergroup, u.g_id, u.name
+			FROM c_members m INNER JOIN c_usergroups u ON (m.usergroup = u.g_id);");
+
 	while($member = $Db->Fetch()) {
 		$member['joined'] = $Core->DateFormat($member['joined']);
-		
+
 		Template::Add("<tr>
 				<td>" . Html::Crop($Core->GetGravatar($member['email'], $member['m_id'], 36), 36, 36) . "</td>
 				<td><b>{$member['username']}</b></td>
 				<td>{$member['email']}</td>
 				<td>{$member['joined']}</td>
 				<td>{$member['name']}</td>
-				<td>{$member['ip_address']}</td>
-				<td><img src=\"images/edit.png\"></td>
-				<td><img src=\"images/delete.png\"></td>
+				<td>{$member['posts']}</td>
+				<td><a href=\"?act=members&amp;p=manage&amp;do=edit&amp;id={$member['m_id']}\"><img src='images/edit.png'></a></td>
+				<td><a href=\"?act=members&amp;p=manage&amp;do=delete&amp;id={$member['m_id']}\"><img src='images/delete.png'></a></td>
 			</tr>");
 	}
-	
+
 ?>
 
 	<h1>Manage Members</h1>
-	
+
 	<div id="content">
-	
 		<div class="grid-row">
 			<!-- LEFT -->
 			<form action="process.php?do=optimize" method="post">
-				
 				<table class="table-list">
 					<tr>
 						<th colspan="8">Registered Members</th>
 					</tr>
-					
 					<tr class="subtitle">
 						<td width="1%"></td>
 						<td>Username</td>
 						<td>E-mail Address</td>
 						<td>Joined</td>
 						<td>Usergroup</td>
-						<td>IP Address</td>
+						<td>Posts</td>
 						<td width="1%"></td>
 						<td width="1%"></td>
 					</tr>
 					<?php echo Template::Get() ?>
-					
 				</table>
-				
 			</form>
 		</div>
-
 	</div>
