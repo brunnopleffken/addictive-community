a89d58c70a3d14969a9a4e7550af9ca242b96ab3
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-03-05 22:01:06 -0300

    Remove features still under evaluation

--- admin/main.php
+++ admin/main.php
@@ -162,9 +162,7 @@ HTML;
 				<a href="main.php?act=members&amp;p=add">Add New Member</a>
 				<a href="main.php?act=members&amp;p=ban">Ban Member</a>
 				<a href="main.php?act=members&amp;p=manage">Manage Members</a>
-				<a href="main.php?act=members&amp;p=ranks">Ranks</a>
 				<a href="main.php?act=members&amp;p=usergroups">User Groups</a>
-				<a href="main.php?act=members&amp;p=subscriptions">Subscriptions</a>
 			</div>
 HTML;
 		}
