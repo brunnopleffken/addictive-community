e7b8e31a05dc4accee6ca31530c4be918998b78c
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-03-26 01:22:29 -0300

    Rewrite Member List

---
+++ controllers/Members.php
@@ -0,0 +1,126 @@
+<?php
+
+## -------------------------------------------------------
+#  ADDICTIVE COMMUNITY
+## -------------------------------------------------------
+#  Created by Brunno Pleffken Hosti
+#  http://github.com/brunnopleffken/addictive-community
+#
+#  File: Members.php
+#  Release: v1.0.0
+#  Copyright: (c) 2015 - Addictive Software
+## -------------------------------------------------------
+
+class Members extends Application
+{
+	/**
+	 * --------------------------------------------------------------------
+	 * MEMBER LIST
+	 * --------------------------------------------------------------------
+	 */
+	public function Main()
+	{
+		$selected = "";
+		$letter_list = "";
+
+		// Yes. The alphabet...
+		$letters = array(
+			"a", "b", "c", "d", "e", "f",
+			"g", "h", "i", "j", "k", "l",
+			"m", "n", "o", "p", "q", "r",
+			"s", "t", "u", "v", "w", "x",
+			"y", "z"
+		);
+
+		// If there is a letter selected, don't let "All" selected
+		if(Html::Request("letter")) {
+			$first = "<a href='members'>" . i18n::Translate("M_ALL") . "</a>\n";
+		}
+		else {
+			$first = "<a href='members' class='page-selected'>" . i18n::Translate("M_ALL") . "</a>\n";
+		}
+
+		// Build letter list
+		foreach($letters as $value) {
+			$label = strtoupper($value);
+			if(Html::Request("letter") == $value) {
+				$selected = "class='page-selected'";
+			}
+			else {
+				$selected = "";
+			}
+
+			if(Html::Request("order")) {
+				$order = "&order=" . $_REQUEST['order'];
+			}
+			else {
+				$order = "";
+			}
+
+			$letter_list .= "<a href='members?letter={$value}{$order}' {$selected}>{$label}</a>\n";
+		}
+
+		// Get member list
+		$results = $this->_GetMemberList();
+
+		// Return variables
+		$this->Set("community_name", $this->config['general_communityname']);
+		$this->Set("letter_first", $first);
+		$this->Set("letter_list", $letter_list);
+		$this->Set("results", $results);
+	}
+
+	/**
+	 * --------------------------------------------------------------------
+	 * GET MEMBER LIST
+	 * --------------------------------------------------------------------
+	 */
+	private function _GetMemberList()
+	{
+		// Declare return variable
+		$_result = array();
+
+		// Sort by username, join date or number of posts
+		if(Html::Request("order")) {
+			switch(Html::Request("order")) {
+				case "join":
+					$order = "ORDER BY joined DESC";
+					break;
+				case "post":
+					$order = "ORDER BY posts DESC";
+					break;
+			}
+		}
+		else {
+			$order = "ORDER BY username";
+		}
+
+		// Search by username
+		if(Html::Request("username")) {
+			$username = Html::Request("username");
+			$order = "WHERE username LIKE '%{$username}%' {$order};";
+		}
+
+		// Filter by first letter and execute query!
+		if(Html::Request("letter")) {
+			$letter = Html::Request("letter");
+			$members = $this->Db->Query("SELECT * FROM c_members
+					LEFT JOIN c_usergroups ON (c_members.usergroup = c_usergroups.g_id)
+					WHERE username LIKE '{$letter}%' {$order};");
+		}
+		else {
+			$members = $this->Db->Query("SELECT * FROM c_members
+					LEFT JOIN c_usergroups ON (c_members.usergroup = c_usergroups.g_id)
+					{$order};");
+		}
+
+		// Iterate between results
+		while($result = $this->Db->Fetch($members)) {
+			$result['avatar'] = $this->Core->GetGravatar($result['email'], $result['photo'], 72, $result['photo_type']);
+			$result['joined'] = $this->Core->DateFormat($result['joined'], "short");
+			$_result[] = $result;
+		}
+
+		return $_result;
+	}
+}
