8a9cb9f1653879cc09ec9bd71653b471e172f177
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-03-27 00:37:22 -0300

    Rewrite Member profile

---
+++ templates/default/Profile.Attachments.php
@@ -0,0 +1,53 @@
+<div class="profile-header">
+	<div class="background" style="background-image: url('<?php __($info['cover']) ?>')"><div class="cover"></div></div>
+	<?php __(Html::Crop($info['avatar'], 160, 160, "avatar")) ?>
+	<div class="member-title"><?php __($info['member_title']) ?></div>
+	<div class="member-info">
+		<span class="username"><?php __($info['username']) ?></span>
+		<div class="item"><span><?php __("P_POSTS") ?></span><?php __($info['posts']) ?></div>
+		<div class="item"><span><?php __("P_REGISTERED") ?></span><?php __($info['joined']) ?></div>
+		<div class="item"><span><?php __("P_USERGROUP") ?></span><?php __($info['name']) ?></div>
+	</div>
+</div>
+
+<div class="navigation">
+	<div class="navbar">
+		<a href="profile/<?php __($member_id) ?>" class="transition selected"><?php __("P_PROFILE") ?></a>
+	</div>
+	<div class="subnav">
+		<a href="profile/<?php __($member_id) ?>"><?php __("P_MY_PROFILE") ?></a>
+		<a href="profile/posts/<?php __($member_id) ?>"><?php __("P_LAST_POSTS") ?></a>
+		<a href="profile/attachments/<?php __($member_id) ?>"><?php __("P_ATTACHMENTS") ?></a>
+	</div>
+</div>
+
+<div class="profile">
+	<div class="box">
+		<!-- ATTACHMENTS -->
+		<table class="table-list no-shadow">
+			<tr>
+				<th colspan="4"><?php __("P_ATTACHMENTS") ?></th>
+			</tr>
+			<tr class="subtitle">
+				<td class="min"></td>
+				<td><?php __("P_NAME") ?></td>
+				<td><?php __("P_FILETYPE") ?></td>
+				<td><?php __("P_FILESIZE") ?></td>
+			</tr>
+			<?php if(!empty($attachments)): ?>
+				<?php foreach($attachments as $file): ?>
+					<tr>
+						<td><?php __($file['icon']) ?></td>
+						<td><?php __($file['filename']) ?></td>
+						<td><?php __($file['type']) ?></td>
+						<td><?php __($file['size']) ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php else: ?>
+				<tr>
+					<td colspan="4" class="center"><?php __("P_NO_ATTACHMENTS") ?></td>
+				</tr>
+			<?php endif; ?>
+		</table>
+	</div>
+</div>
