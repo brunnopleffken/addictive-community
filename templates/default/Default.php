43646b0e9bf6d4aca2ee1a97a2ed03cb9cee3968
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-03-28 12:31:08 -0300

    Select2 path

--- templates/default/Default.php
+++ templates/default/Default.php
@@ -10,7 +10,8 @@
 	<link rel="icon" href="favicon.ico">
 	<!-- CSS -->
 	<link rel="stylesheet" href="templates/default/css/main.css">
-	<link rel="stylesheet" href="thirdparty/Font-Awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="thirdparty/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="thirdparty/select2/select2.css">
 	<!-- JS -->
 	<script src="thirdparty/jquery/jquery.min.js"></script>
 	<script src="thirdparty/tinymce/tinymce.min.js"></script>
