019f656d91be3d24e8cbd2ef4f0a3a9127c06d2e
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-04-09 00:31:02 -0300

    Template for Ajax pages (such as lightboxes)

---
+++ resources/ajax.js
@@ -0,0 +1,165 @@
+/**
+ * ADDICTIVE COMMUNITY
+ * -------------------------------------------------------
+ * Created by Brunno Pleffken Hosti
+ * http://github.com/brunnopleffken/addictive-community
+ *
+ * File: main.js
+ * Release: v1.0.0
+ * Copyright: (c) 2015 - Addictive Software
+ */
+
+
+$(document).ready(function() {
+	/**
+	 * LOGIN - VALIDATE USERNAME AND PASSWORD BEFORE SEND
+	 */
+
+	$('#memberLoginForm').on('submit', function(event) {
+		var error      = false,
+		    $userField = $('#memberLoginForm .username'),
+		    $passField = $('#memberLoginForm .password'),
+		    $submit    = $('#memberLoginForm input[type=submit]'),
+		    timer;
+
+		$.ajax({
+			url: 'login/validate',
+			type: 'post',
+			dataType: 'json',
+			data: { username: $userField.val(), password: $passField.val() }
+		})
+		.done(function(data){
+			if(data.authenticated == 'false') {
+				error = true;
+
+				$userField.addClass('error');
+				$passField.addClass('error');
+
+				$submit.attr('disabled', 'disabled').val(data.message);
+
+				clearTimeout(timer);
+				timer = setTimeout(function() {
+					$('#memberLoginForm input[type=submit]').removeAttr('disabled');
+					$('#memberLoginForm input[type=submit]').val('Log In');
+				}, 2000);
+			}
+		})
+		.fail(function(jqXHR, textStatus) {
+			throw new Error(textStatus);
+		})
+		.always(function() {
+			if(error == false) {
+				var form = document.getElementById('memberLoginForm');
+				form.submit();
+			}
+		});
+
+		event.preventDefault();
+	});
+
+	/**
+	 * CSS CLASSES FOR VALIDATION
+	 * input.url      Validates http://xxx.com or http://xxx.com.br
+	 * input.email    Validates me@me.com or me@me.com.br
+	 * input.numeric  Validates if the value is numeric only
+	 */
+
+	$('form').on('submit', function(event) {
+		if($(this).hasClass('validate')) {
+
+			var stopSend = false;
+
+			// Validade INPUT text, INPUT password and TEXTAREAs
+
+			$(this).find('input[type=text], input[type=password], textarea, select').filter('.required').each(function(){
+				if(this.value == '') {
+					$(this).addClass('error');
+					stopSend = true;
+				}
+				else {
+					$(this).removeClass('error');
+				}
+			});
+
+			// Is the URL valid?
+
+			$(this).find('.url').filter('.required').each(function(){
+				var str = this.value;
+				var pattern = new RegExp(/(http:\/\/)([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2,3}/);
+				var result = pattern.test(str);
+
+				if(!result) {
+					$(this).addClass('error');
+					stopSend = true;
+				}
+				else {
+					$(this).removeClass('error');
+				}
+			});
+
+			// Is the e-mail address valid?
+
+			$(this).find('.email').filter('.required').each(function(){
+				var str = this.value;
+				var pattern = new RegExp(/^[a-z0-9_\.\-]+\@[a-z0-9_\.\-]+\.[a-z]{2,3}$/gm);
+				var result = pattern.test(str);
+
+				if(!result) {
+					$(this).addClass('error');
+					stopSend = true;
+				}
+				else {
+					$(this).removeClass('error');
+				}
+			});
+
+			// Numeric only field
+
+			$(this).find('.numeric').filter('.required').each(function(){
+				var str = this.value;
+				var pattern = new RegExp(/([0-9]*)/);
+				var result = pattern.test(str);
+
+				if(!result) {
+					$(this).addClass('error');
+					stopSend = true;
+				}
+				else {
+					$(this).removeClass('error');
+				}
+			});
+
+			// Alphanumerics ONLY
+
+			$(this).find('.alphanumeric').filter('.required').each(function(){
+				var str = this.value;
+				var pattern = new RegExp(/([a-zA-Z0-9\s]*)/);
+				var result = pattern.test(str);
+
+				if(!result) {
+					$(this).addClass('error');
+					stopSend = true;
+				}
+				else {
+					$(this).removeClass('error');
+				}
+			});
+
+			// IF there is an error, show message!
+			// ...otherwise, send form as it should be
+
+			if(stopSend == true) {
+				event.preventDefault();
+				$(this).find('.error-message').css('display', 'inline-block').hide().fadeIn();
+			}
+		}
+	});
+
+	/**
+	 * REMOVE .error CLASS ON FOCUS (FOR input[type=text] ELEMENTS)
+	 */
+
+	$(document).on('focus', '.error', function() {
+		$(this).removeClass('error');
+	});
+});
---
+++ templates/default/Ajax.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Addictive Community (Powered by Addictive Community)</title>
+	<base href="http://localhost/addictive-community/">
+	<link rel="icon" href="favicon.ico">
+	<!-- CSS -->
+	<link rel="stylesheet" href="templates/default/css/main.css">
+	<link rel="stylesheet" href="thirdparty/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="thirdparty/select2/select2.css">
+	<!-- JS -->
+	<script src="thirdparty/jquery/jquery.min.js"></script>
+	<script src="thirdparty/select2/select2.js"></script>
+	<script src="resources/ajax.js"></script>
+	<?php echo $this->head ?>
+</head>
+<body>
+	<?php echo $this->content ?>
+</body>
+</html>
