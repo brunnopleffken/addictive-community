dee6c83532617a052477519c33892a52dc0d4739
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-04-16 22:44:12 -0300

    Error/message page rewrite

---
+++ controllers/Error.php
@@ -0,0 +1,50 @@
+<?php
+
+## -------------------------------------------------------
+#  ADDICTIVE COMMUNITY
+## -------------------------------------------------------
+#  Created by Brunno Pleffken Hosti
+#  http://github.com/brunnopleffken/addictive-community
+#
+#  File: Error.php
+#  Release: v1.0.0
+#  Copyright: (c) 2015 - Addictive Software
+## -------------------------------------------------------
+
+class Error extends Application
+{
+	/**
+	 * --------------------------------------------------------------------
+	 * VIEW: ERROR PAGE
+	 * --------------------------------------------------------------------
+	 */
+	public function Main()
+	{
+		// Get type of error
+		$type = Html::Request("t");
+
+		// Error list
+		$errors = array(
+			'not_allowed' => array(
+				Html::Notification(i18n::Translate("E_MESSAGE_NOT_ALLOWED"), "warning", true), "login"
+			),
+			'validated' => array(
+				Html::Notification(i18n::Translate("E_MESSAGE_VALIDATED"), "success", true), "login"
+			)
+		);
+
+		// Is this an error, or just a notification message? Change title!
+		if(strpos($errors[$type][0], "success")) {
+			$title = i18n::Translate("E_SUCCESS_TITLE");
+		}
+		else {
+			$title = i18n::Translate("E_ERROR_TITLE");
+		}
+
+		// Return variables
+		$this->Set("community_name", $this->config['general_communityname']);
+		$this->Set("title", $title);
+		$this->Set("error", $errors[$type][0]);
+		$this->Set("action", $errors[$type][1]);
+	}
+}
--- languages/en_US/error.php
+++ languages/en_US/error.php
@@ -1,11 +1,14 @@
 <?php
-	$t["E_TITLE"] = "An error occoured";
+	$t["E_ERROR_TITLE"] = "Ops! An error occoured...";
+	$t["E_SUCCESS_TITLE"] = "Done!";
 	$t["E_USERNAME"] = "Username";
 	$t["E_PASSWORD"] = "Password";
 	$t["E_LOGIN"] = "Log In";
+	$t["E_LOGIN_ERROR"] = "Please, fill in the username and password fields.";
 	$t["E_PROCEED"] = "Proceed";
 	$t["E_WARNING"] = "WARNING";
 	$t["E_SUCCESS"] = "SUCCESS";
+	$t["E_MESSAGE_NOT_ALLOWED"] = "You need to be a registered member to access this page.";
 	$t["E_MESSAGE_VALIDATED"] = "Your e-mail address has been successfully validated.";
 	$t["E_MESSAGE_PROTECTED"] = "This room is protected.";
 	$t["E_MESSAGE_LOGIN_ERROR"] = "Username or password is wrong. Please, try again.";
