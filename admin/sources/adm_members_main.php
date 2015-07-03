589db67289aaa634317b878cd9b6fbd5223bedcc
brunnopleffken <brunno.pleffken@outlook.com>
2015-07-03 08:43:35 -0300

    Edit Admin CP sections explanations

--- admin/sources/adm_members_main.php
+++ admin/sources/adm_members_main.php
@@ -14,18 +14,16 @@
 	<h1>Members Settings</h1>
 
 	<div id="content">
-
 		<div class="grid-row">
 			<!-- LEFT -->
 			<table class="table-list">
 				<tr>
 					<th>Settings Groups</th>
 				</tr>
-				<tr><td><a href="main.php?act=members&p=add"><b>Add New Member</b></a><br>Register a new member manually and define initial settings.</td></tr>
+				<tr><td><a href="main.php?act=members&p=add"><b>Add New Member</b></a><br>Register a new member manually and define their initial settings.</td></tr>
 				<tr><td><a href="main.php?act=members&p=ban"><b>Ban Member</b></a><br>Add or remove member bannishments.</td></tr>
-				<tr><td><a href="main.php?act=members&p=manage"><b>Manage Members</b></a><br>Edit profile and delete members from community.</td></tr>
+				<tr><td><a href="main.php?act=members&p=manage"><b>Manage Members</b></a><br>Manage, edit profiles and delete members from community.</td></tr>
 				<tr><td><a href="main.php?act=members&p=usergroups"><b>User Groups</b></a><br>Add and manage user groups and their settings.</td></tr>
 			</table>
 		</div>
-
 	</div>
--- admin/sources/adm_system_main.php
+++ admin/sources/adm_system_main.php
@@ -23,7 +23,6 @@
 				</tr>
 				<tr><td><a href="main.php?act=system&p=database"><b>Database Toolbox</b></a><br>Perform MySQL tasks and optimization.</td></tr>
 				<tr><td><a href="main.php?act=system&p=logs"><b>Logs</b></a><br>Show administration and moderation logs.</td></tr>
-				<tr><td><a href="main.php?act=system&p=statistics"><b>Statistics</b></a><br>Show community statistics, such as members, posts/threads number, users online, etc.</td></tr>
 				<tr><td><a href="main.php?act=system&p=server"><b>Server Environment</b></a><br>Information about the server, PHPinfo, MySQL version, etc.</td></tr>
 				<tr><td><a href="main.php?act=system&p=optimization"><b>System Optimization</b></a><br>Execute several tasks for community system optimization and fixes.</td></tr>
 			</table>
