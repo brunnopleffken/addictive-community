12fefdee126a6c9f0af5e82f339ba9a17c1a4f96
brunnopleffken <brunno.pleffken@outlook.com>
2015-07-21 12:47:45 -0300

    Convert param from string to boolean (#44)

--- kernel/Email.php
+++ kernel/Email.php
@@ -47,7 +47,7 @@ class Email
 		// Set SMTP configuration
 		$this->Mail->isSMTP();
 		$this->Mail->Host       = $config['general_email_smtp'];
-		$this->Mail->SMTPAuth   = $config['general_email_authentication'];
+		$this->Mail->SMTPAuth   = filter_var($config['general_email_authentication'], FILTER_VALIDATE_BOOLEAN);
 		$this->Mail->Username   = $config['general_email_username'];
 		$this->Mail->Password   = $config['general_email_password'];
 		$this->Mail->SMTPSecure = $config['general_email_auth_method'];
