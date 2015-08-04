3831c890d67fedb58d560412e4191c295242bec6
brunnopleffken <brunno.pleffken@outlook.com>
2015-08-03 21:11:36 -0300

    Brazilian Portuguese translation

---
+++ languages/pt_BR/community.php
@@ -0,0 +1,6 @@
+<?php
+	$t["C_MODERATORS"] = "Moderadores";
+	$t["C_PROTECTED_ROOM"] = "Sala Protegida";
+	$t["C_REDIRECT_TO"] = "Redirecionar para";
+	$t["C_MARK_ALL_READ"] = "Marcar todos como lido";
+?>
---
+++ languages/pt_BR/help.php
@@ -0,0 +1,4 @@
+<?php
+	$t["H_TITLE"] = "Tópicos de Ajuda";
+	$t["H_NO_TOPICS"] = "Não há nenhum tópico de ajuda no momento.";
+?>
