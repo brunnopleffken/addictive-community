09cf3147a38382994eb0ab27cbedd4a182f81607
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-02-02 18:11:32 -0200

    Remove config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// MySQL configuration file for Addictive Community
-$config['db_server']   = "localhost";
-$config['db_username'] = "root";
-$config['db_password'] = "bpleffken";
-$config['db_database'] = "community";
-$config['db_port']     = 3306;
