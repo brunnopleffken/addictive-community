ade095e4a8354f8bdc5b731e1d1ef7abe4d70f91
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-04-13 04:04:05 -0300

    Update Brazilian Portuguese language to v0.9.0 (#84)

--- languages/pt_BR/thread.php
+++ languages/pt_BR/thread.php
@@ -31,8 +31,8 @@
 	$t["T_MESSAGE_3"] = "Você excluiu sua postagem com sucesso.";
 	$t["T_MESSAGE_4"] = "Você fechou este tópico com sucesso.";
 	$t["T_MESSAGE_5"] = "Você reabriu este tópico com sucesso.";
-	$t["T_MESSAGE_6"] = "Você marcou este tópico como anúncio com sucesso.";
-	$t["T_MESSAGE_7"] = "Você removeu este tópico como anúncio com sucesso.";
+	$t["T_MESSAGE_6"] = "Você marcou este tópico como destaque com sucesso.";
+	$t["T_MESSAGE_7"] = "Você removeu este tópico como destaque com sucesso.";
 	$t["P_ADD_REPLY"] = "Adicionar Resposta";
 	$t["P_NEW_THREAD"] = "Novo Tópico";
 	$t["P_EDIT_POST"] = "Editando postagem em";
@@ -44,8 +44,9 @@
 	$t["P_POLL_CHOICES"] = "Alternativas da enquete";
 	$t["P_POLL_CHOICES_DESC"] = "Adicione uma resposta por linha. Máximo de 15 respostas.";
 	$t["P_ATTACHMENTS"] = "Anexos";
+	$t["P_ATTACHMENTS_MAX_SIZE"] = "O tamanho do arquivo não deverá exceder <b>%s</b>.";
 	$t["P_OPTIONS"] = "Opções do tópico";
-	$t["P_SET_ANNOUNCEMENT"] = "Marcar tópico como anúncio";
+	$t["P_SET_ANNOUNCEMENT"] = "Marcar tópico como destaque";
 	$t["P_LOCK"] = "Fechar tópico após postar";
 	$t["P_ERROR"] = "Sua mensagem está vazia.";
 	$t["P_NEW_ERROR"] = "Título e conteúdo da postagem são campos obrigatórios.";
@@ -54,8 +55,13 @@
 	$t["T_MOD_OPTIONS"] = "Opções de Moderação";
 	$t["T_MOD_LOCK_THREAD"] = "Fechar este tópico";
 	$t["T_MOD_UNLOCK_THREAD"] = "Reabrir este tópico";
-	$t["T_MOD_SET_ANNOUNCEMENT"] = "Marcar como anúncio";
-	$t["T_MOD_UNSET_ANNOUNCEMENT"] = "Desmarcar como anúncio";
+	$t["T_MOD_SET_ANNOUNCEMENT"] = "Marcar como destaque";
+	$t["T_MOD_UNSET_ANNOUNCEMENT"] = "Desmarcar como destaque";
 	$t["T_MOD_DELETE_THREAD"] = "Excluir tópico";
 	$t["T_MOD_DELETE_MESSAGE"] = "Você tem certeza que deseja excluir este tópico? Esta ação NÃO poderá ser desfeita!";
+	$t["T_AUTO_TITLE"] = "Definir data de abertura/fechamento automático";
+	$t["T_AUTO_DESCRIPTION"] = "Define quando o tópico será aberto e fechado. Para não utilizar este recurso deixe os campos em branco.";
+	$t["T_AUTO_DETAILS"] = "AVISO: A data de abertura/fechamento é baseada no fuso-horário padrão da comunidade (atualmente definido como <strong>%s horas</strong> no <a href='admin/' target='_blank'>Admin CP</a>), e não no fuso-horário do membro. Isso assegura que todos os membros estarão visualizando o tópico ao mesmo tempo.";
+	$t["T_AUTO_OPEN_AT"] = "Abrir em... <small>(deixe em branco para postar imediatamente)</small>";
+	$t["T_AUTO_CLOSE_AT"] = "E fechar em... <small>(deixe em branco para não definir data de fechamento)</small>";
 ?>
