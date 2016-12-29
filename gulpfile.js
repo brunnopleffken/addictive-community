5b848b0c4123e0c59e093bdc537cb5ca7d60e535
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-12-29 21:02:31 -0200

    Setup gulp and move JS/CSS files to /static

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@ var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 
 gulp.task('styles', () => {
-	gulp.src(['themes/default-light/scss/**/*.scss'])
+	gulp.src(['static/scss/**/*.scss'])
 		.pipe(plumber({
 			errorHandler: function(error) {
 				console.log(error.message);
 				this.emit('end');
 			}
 		}))
-		.pipe(sass({outputStyle: 'compact'}))
-		.pipe(gulp.dest('themes/default-light/css/'))
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(gulp.dest('static/css/'))
 });
 
 gulp.task('default', function(){
-	gulp.watch('themes/default-light/scss/**/*.scss', ['styles']);
+	gulp.watch('static/scss/**/*.scss', ['styles']);
 });
