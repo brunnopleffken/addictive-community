795b1d9eb38644b2c528391898261cf84424084f
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-12-31 03:39:08 -0200

    Optimize Http::Request for integer values
    * If is an integer, explicitly convert to INT type and return

--- kernel/Http.php
+++ kernel/Http.php
@@ -20,18 +20,22 @@ class Http
 	 * SAME AS $_REQUEST['var'], BUT SANITIZED
 	 * --------------------------------------------------------------------
 	 */
-	public static function Request($name, $numeric_only = false)
+	public static function Request($name, $is_integer = false)
 	{
 		if(isset($_REQUEST[$name])) {
 			$text = $_REQUEST[$name];
 
-			// Return error if $numeric_only is true but hasn't a numeric value
-			if($numeric_only == true && !is_numeric($text)) {
-				Html::Error("Variable '{$name}' must be a number.");
+			// Return error if $is_integer is true but hasn't a numeric value
+			if($is_integer) {
+				if(!is_numeric($text)) {
+					Html::Error("Variable '{$name}' must be a number.");
+				}
+
+				return intval($text);
 			}
 
-			// If is not a number, sanitize value as text
-			if(!$numeric_only && !is_array($_REQUEST[$name])) {
+			// If is not a number nor an array (checkboxes), sanitize value as text
+			if(!$is_integer && !is_array($_REQUEST[$name])) {
 				$text = stripslashes($_REQUEST[$name]);
 				$text = str_replace(";", "&semi;", $text);
 				$text = str_replace("& ", "&amp; ", $text);
@@ -41,12 +45,12 @@ class Http
 				$text = str_replace("'", "&apos;", $text);
 				$text = str_replace("`", "&grave;", $text);
 			}
+
+			return $text;
 		}
 		else {
 			return false;
 		}
-
-		return $text;
 	}
 
 	/**
