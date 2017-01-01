bd337b946e686395869d14a09e015080217d1c14
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-01-01 00:39:35 -0200

    Typo

--- languages/en_US/failure.php
+++ languages/en_US/failure.php
@@ -1,5 +1,5 @@
 <?php
-	$t["E_ERROR_TITLE"] = "Ops! An error occoured...";
+	$t["E_ERROR_TITLE"] = "Ops! An error occurred...";
 	$t["E_SUCCESS_TITLE"] = "Done!";
 	$t["E_USERNAME"] = "Username";
 	$t["E_PASSWORD"] = "Password";
@@ -14,13 +14,13 @@
 	$t["E_MESSAGE_LOGIN_ERROR"] = "Username or password is wrong. Please, try again.";
 	$t["E_MESSAGE_LOCKED_THREAD"] = "This thread is locked.";
 	$t["E_OFFLINE_TITLE"] = "Sorry. We're offline!";
-	$t["E_OFFLINE_MESSAGE"] = "%s is currently offline while we performe some maintenance.";
+	$t["E_OFFLINE_MESSAGE"] = "%s is currently offline while we perform some maintenance.";
 	$t["E_UPDATING"] = "Updating!";
 	$t["E_UPDATING_MESSAGE"] = "Please, wait while we update our community to a newer software version.";
 	$t["E_DELETED"] = "This member doesn't exist!";
 	$t["E_DELETED_MESSAGE"] = "Seems like you're trying to view a member that doesn't exist or has been deleted.";
 	$t["E_NOT_FOUND"] = "The requested page was not found.";
 	$t["E_NOT_FOUND_MESSAGE"] = "Also known as \"Error 404\", seems like the page you're trying to visit doesn't exist.";
-	$t["E_SERVER_ERROR"] = "An unexpected error occoured.";
+	$t["E_SERVER_ERROR"] = "An unexpected error occurred.";
 	$t["E_SERVER_ERROR_MESSAGE"] = "This may have occurred because of an internal server error.<br>If the error persists contact the community administrator.";
 ?>
