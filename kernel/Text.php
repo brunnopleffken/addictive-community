95c3dd34b0ae39d3a8edbceb2a8c8224a2d61e12
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-01-08 23:49:41 -0200

    Fix: Set root namespace when declaring Transliterator

--- kernel/Text.php
+++ kernel/Text.php
@@ -169,7 +169,7 @@ class Text
 	 */
 	public static function Slug($string, $replacement = "-")
 	{
-		$transliteration = Transliterator::create("Any-Latin; Latin-ASCII; Lower()");
+		$transliteration = \Transliterator::create("Any-Latin; Latin-ASCII; Lower()");
 		$string = $transliteration->transliterate($string);
 		$string = preg_replace("/[^a-zA-Z0-9\s]/", "", $string);
 		$string = preg_replace("/[\s]+/", $replacement, $string);
