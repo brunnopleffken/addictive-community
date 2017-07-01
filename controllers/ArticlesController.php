4ef31dc7e1260d7d83828abe4e9deded612ffb07
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-07-01 01:15:16 -0300

    Create ArticlesController

---
+++ controllers/ArticlesController.php
@@ -0,0 +1,33 @@
+<?php
+
+## -------------------------------------------------------
+#  ADDICTIVE COMMUNITY
+## -------------------------------------------------------
+#  Created by Brunno Pleffken Hosti
+#  https://github.com/addictivehub/addictive-community
+#
+#  File: Articles.php
+#  License: GPLv2
+#  Copyright: (c) 2017 - Addictive Community
+## -------------------------------------------------------
+
+namespace AC\Controllers;
+
+use \AC\Kernel\Database;
+use \AC\Kernel\i18n;
+use \AC\Kernel\Session\SessionState;
+use \AC\Kernel\Text;
+
+class Articles extends Application
+{
+	/**
+	 * --------------------------------------------------------------------
+	 * ARTICLES HOME
+	 * --------------------------------------------------------------------
+	 */
+	public function index()
+	{
+		echo "OK";
+		exit;
+	}
+}
