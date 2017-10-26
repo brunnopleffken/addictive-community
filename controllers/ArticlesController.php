cbd92ab928a7d3ac9c44b478d5c9a955fc1ec0c5
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-10-25 22:50:12 -0200

    Create and save new articles

--- controllers/ArticlesController.php
+++ controllers/ArticlesController.php
@@ -14,9 +14,11 @@
 namespace AC\Controllers;
 
 use \AC\Kernel\Database;
+use \AC\Kernel\Http;
 use \AC\Kernel\i18n;
 use \AC\Kernel\Session\SessionState;
 use \AC\Kernel\Text;
+use \AC\Kernel\Upload;
 
 class Articles extends Application
 {
@@ -40,5 +42,59 @@ class Articles extends Application
 	public function read($id)
 	{
 		$this->Set('teste', $id);
+	}
+
+	/**
+	 * --------------------------------------------------------------------
+	 * CREATE NEW ARTICLE
+	 * --------------------------------------------------------------------
+	 */
+	public function add()
+	{
+		// Do not allow guests
+		SessionState::noGuest();
+
+		// Get categories
+		Database::query("SELECT * FROM c_article_categories;");
+		$categories = Database::fetchToArray();
+
+		// Variables
+		$page_info['title'] = i18n::translate("articles.add.title");
+		$page_info['bc'] = array(i18n::translate("articles.title"), i18n::translate("articles.add.title"));
+		$this->Set("page_info", $page_info);
+
+		$this->Set("categories", $categories);
+	}
+
+	/**
+	 * --------------------------------------------------------------------
+	 * SAVE NEW ARTICLE IN DATABASE
+	 * --------------------------------------------------------------------
+	 */
+	public function save()
+	{
+		// This page has no layout
+		$this->layout = false;
+
+		// Format new article array
+		$article = [
+			"member_id"   => SessionState::$user_data['m_id'],
+			"title"       => Http::Request("title"),
+			"slug"        => Text::slug(htmlspecialchars_decode(Http::request("title"), ENT_QUOTES)),
+			"content"     => str_replace("'", "&apos;", $_POST['post']),
+			"post_date"   => time()
+		];
+
+		// Send attachments
+		if(Http::getFile("post_image")) {
+			$Upload = new Upload();
+			$post['post_image'] = $Upload->sendAttachment(Http::getFile("post_image"), $article['member_id']);
+		}
+
+		// Insert into database
+		Database::insert("c_articles", $article);
+
+		// Redirect
+		$this->Core->redirect("articles");
 	}
 }
