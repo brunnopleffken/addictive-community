5bfe3aa6d65b66d33b27ca1c1c57dde62247c2df
Brunno Pleffken <brunno.pleffken@outlook.com>
2018-02-21 16:43:35 -0300

    Merge framework.css and wireframe.css into one file

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,26 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('styles', () => {
-	gulp.src(['static/scss/**/*.scss'])
+	let files = [
+		'static/scss/framework.scss',
+		'static/scss/wireframe.scss'
+	];
+
+	gulp.src(files)
 		.pipe(plumber({
 			errorHandler: function(error) {
 				console.log(error.message);
 				this.emit('end');
 			}
 		}))
-		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(concat('app.min.scss'))
+		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(gulp.dest('static/css/'))
 });
 
 gulp.task('default', function(){
-	gulp.watch('static/scss/**/*.scss', ['styles']);
+	gulp.watch('static/scss/*.scss', ['styles']);
 });
